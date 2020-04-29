e6cbd938f7aa0b3c1ebe739086c36b6911536266
user <user@Williams-iMac.local>
2020-04-29 20:32:56 +0500

    Added Class Client

--- Program.cs
+++ Program.cs
@@ -1,15 +1,37 @@
 ﻿using System;
 using System.Threading;
+using System.Collections.Generic;
 
 namespace HomeWork15
 {
+    
     class Program
     {
+        public static List<Client> ClientList = new List<Client>();
         static void Main(string[] args)
         {
-           
+            
         }
+        
+    }
+    class Client
+    {
+        
+        public int ID { get; set; }
+        public decimal Balance { get; set; }
+
+        public Client()
+        {
+
+        }
+        public Client(int ID, decimal Balance)
+        {
+            this.ID = ID;
+            this.Balance = Balance;
+        }
+        
+
     }
 
-   
+    
 }
